d370e2e7f51534966eccf7fa8231d9b6b67b5089
Brady Turner <bradyaturner@gmail.com>
2017-12-01 19:26:11 -0500

    Day 01, Part 2, solved

--- day01/solution.rb
+++ day01/solution.rb
@@ -6,11 +6,32 @@
 # the list.
 
 file = File.read "input.txt"
-@sum = 0
-characters = file.delete("\n").split("")
-characters.each_with_index do |c,i|
-  i2 = (i==characters.length-1) ? 0 : i+1
-  x = characters[i2]
-  @sum += c.to_i if c == x
+@characters = file.delete("\n").split("")
+
+def partone
+  sum = 0
+  @characters.each_with_index do |c,i|
+    i2 = (i==@characters.length-1) ? 0 : i+1
+    x = @characters[i2]
+    sum += c.to_i if c == x
+  end
+  puts "Part 1 Solution: #{sum}"
 end
-puts @sum
+
+# Now, instead of considering the next digit, it wants you to consider the
+# digit halfway around the circular list. That is, if your list contains 10
+# items, only include a digit in your sum if the digit 10/2 = 5 steps forward
+# matches it. Fortunately, your list has an even number of elements.
+
+def parttwo
+  sum = 0
+  @characters.each_with_index do |c,i|
+    i2 = (i+@characters.length/2)%@characters.length
+    x = @characters[i2]
+    sum += c.to_i if c==x
+  end
+  puts "Part 2 Solution: #{sum}"
+end
+
+partone
+parttwo
