3aa95c26136fb2e54e3cf2a71611ab4d8d6deb14
Brady Turner <bradyaturner@gmail.com>
2017-12-01 19:58:37 -0500

    Day 01, refactored.

---
+++ day01/README.md
@@ -0,0 +1,14 @@
+DAY ONE, PART ONE
+
+The captcha requires you to review a sequence of digits (your puzzle input)
+and find the sum of all digits that match the next digit in the list. The
+list is circular, so the digit after the last digit is the first digit in
+the list.
+
+DAY ONE, PART TWO
+
+Now, instead of considering the next digit, it wants you to consider the
+digit halfway around the circular list. That is, if your list contains 10
+items, only include a digit in your sum if the digit 10/2 = 5 steps forward
+matches it. Fortunately, your list has an even number of elements.
+
--- day01/solution.rb
+++ day01/solution.rb
@@ -1,37 +1,31 @@
 #!/usr/bin/env ruby
 
-# The captcha requires you to review a sequence of digits (your puzzle input)
-# and find the sum of all digits that match the next digit in the list. The
-# list is circular, so the digit after the last digit is the first digit in
-# the list.
-
-file = File.read "input.txt"
-@characters = file.delete("\n").split("")
-
-def partone
-  sum = 0
-  @characters.each_with_index do |c,i|
-    i2 = (i==@characters.length-1) ? 0 : i+1
-    x = @characters[i2]
-    sum += c.to_i if c == x
+class DayOneSolver
+  def initialize(path)
+    @characters = File.read(path).delete("\n").split("")
+    @sum = 0
   end
-  puts "Part 1 Solution: #{sum}"
+
+  def solve
+    @characters.each_with_index do |c,i|
+      @sum += c.to_i if c==@characters[get_compared(i)]
+    end
+    puts "Solution: #{@sum}" 
+  end
 end
 
-# Now, instead of considering the next digit, it wants you to consider the
-# digit halfway around the circular list. That is, if your list contains 10
-# items, only include a digit in your sum if the digit 10/2 = 5 steps forward
-# matches it. Fortunately, your list has an even number of elements.
-
-def parttwo
-  sum = 0
-  @characters.each_with_index do |c,i|
-    i2 = (i+@characters.length/2)%@characters.length
-    x = @characters[i2]
-    sum += c.to_i if c==x
-  end
-  puts "Part 2 Solution: #{sum}"
+class D1P1Solver < DayOneSolver
+  def get_compared(i)
+    (i==@characters.length-1) ? 0 : i+1
+  end 
 end
 
-partone
-parttwo
+class D1P2Solver < DayOneSolver
+  def get_compared(i)
+    (i+@characters.length/2) % @characters.length
+  end
+end
+
+path = "input.txt"
+D1P1Solver.new(path).solve
+D1P2Solver.new(path).solve
