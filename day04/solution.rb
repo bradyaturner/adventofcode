7a2f825f5c1d68fcda16c10f8d3c80a0142b323e
Brady Turner <bturner@nuvotechnologies.com>
2017-12-04 09:59:19 -0500

    Day 04, Part 2, solved

--- day04/solution.rb
+++ day04/solution.rb
@@ -9,9 +9,8 @@ class DayFourSolver
     num_valid = 0
     @file.each_line do |line|
       valid = true
-      words = line.split
       counts = Hash.new 0
-      words.each { |word| counts[word] += 1 }
+      line.split.each { |word| counts[get_word(word)] += 1 }
       counts.each { |word,count| valid = false if count > 1 }
       num_valid += 1 if valid
     end
@@ -19,5 +18,16 @@ class DayFourSolver
   end
 end
 
+class D4P1Solver < DayFourSolver
+  def get_word(word) word end
+end
+
+class D4P2Solver < DayFourSolver
+  def get_word(word)
+    word.chars.sort.join
+  end
+end
+
 path = "input.txt"
-DayFourSolver.new(path).solve
+D4P1Solver.new(path).solve
+D4P2Solver.new(path).solve
