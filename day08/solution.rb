eb75811f825be9b207f0a01bb09e06a94a5651c3
Brady Turner <bturner@nuvotechnologies.com>
2017-12-08 13:06:59 -0500

    Cleanup

--- day08/solution.rb
+++ day08/solution.rb
@@ -4,43 +4,36 @@ class DayEightSolver
   def initialize(path)
     @path = path
     @file = File.read @path
+    @registers = Hash.new 0
     @historical_max = 0
     interpret_file
   end
 
   def interpret_file
-    @registers = Hash.new 0
     @file.each_line {|line| parse_instruction line}
   end
 
   def parse_instruction(ins)
     parts = ins.split
-
-    reg = parts[0]
-    op = parts[1]
-    amt = parts[2].to_i
-
-    # conditional
-    creg = parts[4]
-    comp = parts[5]
-    cval = parts[6].to_i
-
+    reg, op, amt = parts[0], parts[1], parts[2].to_i
+    creg, comp, cval = parts[4], parts[5], parts[6].to_i
     eval("@registers[reg] #{op=="inc" ? "+=":"-="} amt if @registers[creg] #{comp} cval")
-    @historical_max = @registers.values.max if @registers.values.length > 0 && @registers.values.max > @historical_max
   end
 end
 
 class D8P1Solver < DayEightSolver
   def solve
-    @solution = @registers.values.max
-    puts "P1 Solution(#{@path}): #{@solution}\n\n"
+    puts "P1 Solution(#{@path}): #{@registers.values.max}\n\n"
   end
 end
 
 class D8P2Solver < DayEightSolver
   def solve
-    @solution = @historical_max
-    puts "P2 Solution(#{@path}): #{@solution}\n\n"
+    puts "P2 Solution(#{@path}): #{@historical_max}\n\n"
+  end
+  def parse_instruction(ins)
+    super
+    @historical_max = @registers.values.max if (@registers.values.max||0) > @historical_max
   end
 end
 
